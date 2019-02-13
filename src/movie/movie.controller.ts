dd826c3619ead67ca53b7733d8971faadf866b8d
gosuhiman <gosuhiman@gmail.com>
2019-02-13 23:15:48 +0100

    all exception filter

--- src/movie/movie.controller.ts
+++ src/movie/movie.controller.ts
@@ -1,4 +1,4 @@
-import {Body, Controller, Delete, ForbiddenException, Get, Param, Patch, Post} from '@nestjs/common';
+import {Body, Controller, Delete, Get, Param, Patch, Post} from '@nestjs/common';
 import {CreateMovieDTO} from './dto/create-movie.dto';
 import {UpdateMovieDTO} from './dto/update-movie.dto';
 import {Movie} from './movie.entity';
