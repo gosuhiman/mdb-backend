57141baf724b51038ebe72251c1da1c45a979f70
gosuhiman <gosuhiman@gmail.com>
2019-02-18 01:20:09 +0100

    cors

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
-import * as dotenv from 'dotenv';
-dotenv.config();
-
 import {AllExceptionsFilter} from '@error/all-exception.filter';
 import {ValidationPipe} from '@nestjs/common';
 import {HTTP_SERVER_REF, NestFactory} from '@nestjs/core';
+import * as dotenv from 'dotenv';
 import {AppModule} from './app.module';
 
+dotenv.config();
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {cors: true});
   const httpRef = app.get(HTTP_SERVER_REF);
   app.useGlobalFilters(new AllExceptionsFilter(httpRef));
   app.useGlobalPipes(new ValidationPipe({
